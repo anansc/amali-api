25ec938345ec34d6e1c13d876ef2a8b4b640ea08
Ana <anahaydn3@gmail.com>
2021-11-21 02:01:52 -0300

    init

---
+++ src/test/java/com/meta/amaliapi/AmaliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.amaliapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
