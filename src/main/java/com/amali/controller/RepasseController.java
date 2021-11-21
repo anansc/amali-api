963f5977d14dbfeb99ef0295b96951670792469a
Ana <anahaydn3@gmail.com>
2021-11-21 09:00:05 -0300

    testando page e etc

--- src/main/java/com/amali/controller/RepasseController.java
+++ src/main/java/com/amali/controller/RepasseController.java
@@ -7,6 +7,8 @@ import com.amali.model.Repasse;
 import com.amali.repository.RepasseRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +26,19 @@ public class RepasseController {
 	    List<Repasse> result = repasseRepository.findAll();
 	    return ResponseEntity.ok(result);
 	}
+	@GetMapping(value ="/page")
+	public ResponseEntity<Page<Repasse>> findAll(Pageable pageable) {
+	    Page<Repasse> result = repasseRepository.findAll(pageable);
+	    return ResponseEntity.ok(result);
+	}
 	
 	@GetMapping(value = "/lchart")
-	public ResponseEntity<List<Repasse>> searchByMunicipio(
-			@RequestParam(defaultValue = "") String municipio)
+	public ResponseEntity<Page<Repasse>> searchByMunicipio(
+			@RequestParam(defaultValue = "") String municipio,
+			@RequestParam(defaultValue = "") String esferaGoverno,
+			Pageable pageable)
 	{
-		List<Repasse> result =  repasseRepository.searchMunicipio(municipio);
+		Page<Repasse> result =  repasseRepository.searchMunicipio(municipio, esferaGoverno, pageable);
 		return ResponseEntity.ok(result);
 	}
 
--- src/main/java/com/amali/repository/RepasseRepository.java
+++ src/main/java/com/amali/repository/RepasseRepository.java
@@ -2,6 +2,8 @@ package com.amali.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +14,8 @@ import com.amali.model.Repasse;
 @Repository
 public interface RepasseRepository extends JpaRepository<Repasse, Long> {
 
-	@Query("SELECT obj FROM Repasse obj WHERE LOWER(obj.municipio) LIKE LOWER(:municipio)")
-	List<Repasse> searchMunicipio(@Param("municipio") String municipio);
+	@Query("SELECT obj FROM Repasse obj WHERE LOWER(obj.municipio) LIKE LOWER(:municipio) AND LOWER(obj.esferaGoverno) LIKE LOWER(:esferaGoverno)")
+	Page<Repasse> searchMunicipio(@Param("municipio") String municipio,
+			@Param("esfera") String esferaGoverno, Pageable pageable);
 
 }
