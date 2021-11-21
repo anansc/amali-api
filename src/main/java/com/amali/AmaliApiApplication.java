f028d378f2eeb184017a93ec3ac470e01fa9ecc5
Ana <anahaydn3@gmail.com>
2021-11-21 02:18:13 -0300

    init

---
+++ src/main/java/com/amali/AmaliApiApplication.java
@@ -0,0 +1,13 @@
+package com.amali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaliApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaliApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amali/AmaliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
