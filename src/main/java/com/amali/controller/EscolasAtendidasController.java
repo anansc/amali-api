260f8c0cd2f420da2e85c004ff9fc7fc852270f5
Ana <anahaydn3@gmail.com>
2021-12-05 07:51:44 -0300

    add route escolas

--- src/main/java/com/amali/controller/EscolasAtendidasController.java
+++ src/main/java/com/amali/controller/EscolasAtendidasController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.amali.model.EscolasAtendidas;
 import com.amali.repository.EscolasAtendidasRepository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/escolas_atendidas")
@@ -31,6 +33,17 @@ public class EscolasAtendidasController {
 	    Page<EscolasAtendidas> result = escolasAtendidasRepository.findAll(pageable);
 	    return ResponseEntity.ok(result);
 	}
+	
+	@GetMapping(value ="/pMunicipio")
+	public ResponseEntity<List<EscolasAtendidas>> findOrderByMunicipioEsfera(
+			@RequestParam(defaultValue = "") String municipio,
+			@RequestParam(defaultValue = "") String esferaGoverno,
+			Sort sort
+			) {
+	    List<EscolasAtendidas> result = escolasAtendidasRepository.searchMunicipioEsfera(municipio, esferaGoverno,(Sort.by(Sort.Direction.ASC,"municipio")));
+	    return ResponseEntity.ok(result);
+	}
+	
 	
 	
 	
--- src/main/java/com/amali/repository/EscolasAtendidasRepository.java
+++ src/main/java/com/amali/repository/EscolasAtendidasRepository.java
@@ -1,6 +1,11 @@
 package com.amali.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.amali.model.EscolasAtendidas;
@@ -8,4 +13,8 @@ import com.amali.model.EscolasAtendidas;
 @Repository
 public interface EscolasAtendidasRepository extends JpaRepository<EscolasAtendidas, Long> {
 
+	
+	@Query("SELECT obj FROM EscolasAtendidas obj WHERE LOWER(obj.municipio) LIKE LOWER(:municipio) AND LOWER(obj.esferaGoverno) LIKE LOWER(:esferaGoverno)")
+	List<EscolasAtendidas> searchMunicipioEsfera(@Param("municipio") String municipio,
+			@Param("esferaGoverno") String esferaGoverno, Sort sort);
 }
