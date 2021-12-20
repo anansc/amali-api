27781bd7665a40055feee8edaeb61b7c599e530e
Ana <anahaydn3@gmail.com>
2021-12-20 10:41:47 -0300

    att

--- src/main/java/com/amali/controller/AlunosController.java
+++ src/main/java/com/amali/controller/AlunosController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amali.model.Alunos;
-import com.amali.model.Repasse;
 import com.amali.repository.AlunosRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
